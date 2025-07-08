81987feba060b8f5b5b26241320592dd21cf3f18
kareem358 <kareemullahafridi10@gmail.com>
2025-07-08 23:49:17 +0500

    Update login_screeen.dart

--- lib/ui/auth/login_screeen.dart
+++ lib/ui/auth/login_screeen.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../../widgets/round_button.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -9,14 +11,102 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: Colors.deepPurple,
+        //Theme.of(context).colorScheme.primary,
         automaticallyImplyLeading: false,
         centerTitle: true,
-        title: Text("Login", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        title: Text("Login", style: TextStyle(fontSize: 20,
+            color: Colors.white,fontWeight: FontWeight.bold),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(key: _formKey,
+                child:Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        // obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          //helperText: "Enter Your Email  e.g tech@gmail.com",
+                          hintText: "Enter your email",
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(11),
+                          ),
+                          filled: true,
+                          fillColor: Theme.of(context).colorScheme.surfaceVariant,
+                        ),
+                      validator: (value){
+                          if(value!.isEmpty){
+                            return "enter your email";
+                          }
+                          return null;
+                      },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: "Password",
+                            hintText: "Enter Your Password",
+                            prefixIcon: Icon(Icons.password_outlined),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(11),
+                            )
+
+                        ),
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return "enter your password";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ) ),
+
+
+            SizedBox(
+              height: 11,
+            ),
+            RoundButton(title: "Login",
+            onTap: () {
+              if(_formKey.currentState!.validate()){};
+            },
+            ),
+          ],
+        ),
       ),
     );
   }
