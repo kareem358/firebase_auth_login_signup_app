31e443e61fd42c2bd42dc051e535b0ac22b136d3
kareem358 <kareemullahafridi10@gmail.com>
2025-07-08 16:05:10 +0500

    Update splash_services.dart

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,7 +1,16 @@
 
 
-class SplashService{
-   void isLogin(){
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+
+import '../ui/auth/login_screeen.dart';
+
+class SplashService{
+   void isLogin(BuildContext context){
+
+      Timer(Duration (seconds: 3), (){
+         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+      });
    }
 }
