a25fad8e82e59e212a240f809e992716dd62ba31
kareem358 <kareemullahafridi10@gmail.com>
2025-07-08 16:04:52 +0500

    Update splash_screen.dart

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_csplash_login/firebase_services/splash_services.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,20 +11,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+SplashService splashService=SplashService();
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    splashService.isLogin(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+     /* appBar: AppBar(
         title: const Text("FireBase Splash Screen"),
         centerTitle: true,
-      ),
+      ),*/
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(Icons.flutter_dash,size: 100,),
-            Text("Welcome to firebase Tutorial",style: TextStyle(fontSize: 22,
-            fontWeight: FontWeight.bold),),
+            const Icon(Icons.flutter_dash, size: 100, color: Colors.blue),
+            const SizedBox(height: 20),
+            const Text(
+              "Welcome to Firebase Tutorial",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
           ],
         ),
       ),
