c11563d2bf53f2a0e447b841b9467d4b74e30fa8
kareem358 <kareemullahafridi10@gmail.com>
2025-07-10 07:11:06 +0500

    Update utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -4,10 +4,43 @@ import 'package:flutter/material.dart';
 class Utils {
   static void toastMessage(String message) {
     Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+  }
+  static void showCustomToast({
+    required String message,
+    required Color backgroundColor,
+    Color textColor = Colors.white,
+    Duration duration = const Duration(seconds: 2),
+  }) {
+    Fluttertoast.showToast(
       msg: message,
-      backgroundColor: Colors.red.shade200,
+      backgroundColor: Colors.red.shade700,
       textColor: Colors.white,
       toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+  static void showSuccessToast(String message) {
+    _showToast(message, Colors.green);
+  }
+
+  static void showErrorToast(String message) {
+    _showToast(message, Colors.red);
+  }
+
+  static void _showToast(String message, Color color) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: color,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.BOTTOM,
     );
   }
