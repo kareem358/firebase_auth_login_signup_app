07f3005e6f631c7b83e45fa19a81b068fd808aa1
kareem358 <kareemullahafridi10@gmail.com>
2025-07-10 07:52:19 +0500

    Update post_screen.dart

---
+++ lib/ui/posts/post_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PostScreen extends StatefulWidget {
+  const PostScreen({super.key});
+
+  @override
+  State<PostScreen> createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Post Screen"),
+        centerTitle: true,
+      ) ,
+      body: Center(
+        child: Text("Post Screen"),
+      ),
+    );
+  }
+}
