546cf8a58eef1705890340aa66f6eeb810ecbdd7
kareem358 <kareemullahafridi10@gmail.com>
2025-07-15 22:33:16 +0500

    Create add_posts.dart

---
+++ lib/ui/posts/add_posts.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Post"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 30,)
+        ],
+      ),
+    );
+  }
+}
