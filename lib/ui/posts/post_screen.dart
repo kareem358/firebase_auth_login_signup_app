2dc42bac30b4e6e24293e14d1dfc6ddea927947c
kareem358 <kareemullahafridi10@gmail.com>
2025-07-15 21:52:31 +0500

    Update post_screen.dart

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_csplash_login/ui/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -8,6 +11,7 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth =FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +19,33 @@ class _PostScreenState extends State<PostScreen> {
         backgroundColor: Colors.deepPurple,
         title: Text("Post Screen",style: TextStyle(fontSize: 22,)),
         centerTitle: true,
+        actions: [
+
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () {
+                auth.signOut().then((_) {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                        (route) => false, // Removes all previous routes
+                  );
+                }).catchError((e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Logout failed: ${e.toString()}')),
+                  );
+                });
+              },
+            ),
+
+         /* IconButton(onPressed: (){
+            auth.signOut().then(value){
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => LoginScreen(),));
+            }
+          },
+              icon: Icon(Icons.logout))*/
+        ],
       ) ,
       body: Center(
         child: Text("Post Screen",style: TextStyle(fontSize: 22),),
