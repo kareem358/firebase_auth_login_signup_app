a3316e901074d2baeb28aa7749561a75125cbcea
kareem358 <kareemullahafridi10@gmail.com>
2025-07-19 22:00:07 +0500

    Update post_screen.dart

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:firebase_csplash_login/ui/auth/login_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 import '../../utils/utils.dart';
@@ -15,6 +17,7 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth =FirebaseAuth.instance;
+  final ref=FirebaseDatabase.instance.ref('Post');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +40,9 @@ class _PostScreenState extends State<PostScreen> {
                 },
                icon: Icon(Icons.logout, color: Colors.white,),
                ),
-
-
-
-
-
         ],
       ) ,
+
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
             Navigator.push(
@@ -66,9 +65,28 @@ class _PostScreenState extends State<PostScreen> {
             borderRadius: BorderRadius.circular(16),
           ),
         ),
-      body: Center(
-        child: Text("Post Screen",style: TextStyle(fontSize: 22),),
-      )
+      body:Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+
+                query: ref,
+                defaultChild: Text("Loading "),
+                itemBuilder:
+                    ( context,  snapshot,  animation, int index) {
+
+            
+                  return ListTile(
+                    title: Text(snapshot.child('title').value.toString()),
+                    subtitle: Text(snapshot.child('id').value.toString()),
+                  );
+                    }
+            
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
