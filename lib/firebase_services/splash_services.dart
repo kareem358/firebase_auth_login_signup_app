0351f34266d827574b2e2c65bc296eff6ccbc51d
kareem358 <kareemullahafridi10@gmail.com>
2025-07-26 02:13:55 +0500

    Update splash_services.dart

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../firestore/firestore_list_screen.dart';
 import '../ui/auth/login_screen.dart';
 import '../ui/posts/post_screen.dart';
 
@@ -15,7 +16,9 @@ class SplashService {
             // already logged in
             Navigator.pushReplacement(
                context,
-               MaterialPageRoute(builder: (_) => const PostScreen()),
+               //now for firestore instead of working with PostScreen user
+               // will be redirected to the FireStoreScreen
+               MaterialPageRoute(builder: (_) => const FireStoreScreen()),
             );
          } else {
             // No user is logged in
