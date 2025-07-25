47d52cf96d2521f3e1ddce8751cf6974fc8c9a08
kareem358 <kareemullahafridi10@gmail.com>
2025-07-26 02:14:01 +0500

    Update firestore_list_screen.dart

---
+++ lib/firestore/firestore_list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class FireStoreScreen extends StatefulWidget {
+  const FireStoreScreen({super.key});
+
+  @override
+  State<FireStoreScreen> createState() => _FireStoreScreenState();
+}
+
+class _FireStoreScreenState extends State<FireStoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
