d60ce862fac12aaadedb8782f0650063b87b115a
kareem358 <kareemullahafridi10@gmail.com>
2025-08-11 22:02:55 +0500

    Update splash_services.dart

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -34,25 +34,4 @@ class SplashService {
 
 
 
-/*
 
-
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-
-import '../ui/auth/login_screen.dart';
-
-class SplashService{
-   void isLogin(BuildContext context){
-
-      Future<void> isLogin(BuildContext context) async {
-         await Future.delayed(const Duration(seconds: 2)); // splash delay
-         // your login check logic here (e.g. using SharedPreferences or FirebaseAuth)
-      }
-
-      Timer(Duration (seconds: 2), (){
-         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-      });
-   }
-}*/
